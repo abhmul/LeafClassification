b720fe72aa5efb55543c6313d8532b23e9b01341
abhmul <abhmul@gmail.com>
2016-12-16 21:42:39 -0600

    Added k_best functionality and changed verbosity to 0

--- src/kfold_train.py
+++ src/kfold_train.py
@@ -23,64 +23,86 @@ n_folds = 6
 augment = False
 nbr_aug = 10 if augment else 1
 stratified = True
+top_k = 4
+threshold = .01
 
 kf, (X_num_tr, X_img_tr, y_tr) = leaf99.load_train_data_kfold(n_folds=n_folds, random_state=split_random_state, stratified=stratified)
 
 y_tr_cat = to_categorical(y_tr)
 assert(y_tr_cat.shape[1] == len(leaf99.LABELS))
 
-print('Initializing Data Augmenter...')
-imgen = ImageDataGenerator2(
-    rotation_range=10,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    vertical_flip=True,
-    fill_mode='nearest')
-print('Data Augmenter Initialized.')
 
-histories = []
+k_best = range(top_k)
+imgen = None
 
-# Start the kfold
-for i, (train_index, test_index) in enumerate(kf):
+while True:
 
-    # Get the fold dataset
-    X_num_tr_fold = X_num_tr[train_index]
-    X_img_tr_fold = X_img_tr[train_index]
-    y_tr_cat_fold = y_tr_cat[train_index]
-    X_num_val_fold = X_num_tr[test_index]
-    X_img_val_fold = X_img_tr[test_index]
-    y_val_cat_fold = y_tr_cat[test_index]
+    print('Initializing Data Augmenter...')
+    imgen = ImageDataGenerator2(
+        rotation_range=10,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        vertical_flip=True,
+        fill_mode='nearest')
+    print('Data Augmenter Initialized.')
+    min_val_loss = []
 
-    # Flow the augmenter
-    fold_imgen = imgen.flow(X_img_tr_fold, y_tr_cat_fold)
+    # Start the kfold
+    for i, (train_index, test_index) in enumerate(kf):
 
-    print('Starting KFold number {}/{}'.format(i+1, n_folds))
-    print('Split train: ', len(X_num_tr_fold), len(X_img_tr_fold), len(y_tr_cat_fold))
-    print('Split valid: ', len(X_num_val_fold), len(X_img_val_fold), len(y_val_cat_fold))
+        # Get the fold dataset
+        X_num_tr_fold = X_num_tr[train_index]
+        X_img_tr_fold = X_img_tr[train_index]
+        y_tr_cat_fold = y_tr_cat[train_index]
+        X_num_val_fold = X_num_tr[test_index]
+        X_img_val_fold = X_img_tr[test_index]
+        y_val_cat_fold = y_tr_cat[test_index]
 
-    print('Creating the model...')
-    model = best_combined_model()
-    print('Model created!')
+        # Flow the augmenter
+        fold_imgen = imgen.flow(X_img_tr_fold, y_tr_cat_fold)
 
-    # autosave best Model
-    best_model_file = "../models/leafnet_v1.1_fold{}.h5".format(i+1)
-    best_model = ModelCheckpoint(best_model_file, monitor='val_loss', verbose=1, save_best_only=True)
+        print('Starting KFold number {}/{}'.format(i+1, n_folds))
+        print('Split train: ', len(X_num_tr_fold), len(X_img_tr_fold), len(y_tr_cat_fold))
+        print('Split valid: ', len(X_num_val_fold), len(X_img_val_fold), len(y_val_cat_fold))
 
-    print('Training model...')
-    histories.append(model.fit_generator(combined_generator(fold_imgen, X_num_tr_fold),
-                                         samples_per_epoch=X_num_tr_fold.shape[0],
-                                         nb_epoch=100,
-                                         validation_data=([X_img_val_fold, X_num_val_fold], y_val_cat_fold),
-                                         nb_val_samples=X_num_val_fold.shape[0],
-                                         verbose=1,
-                                         callbacks=[best_model]))
+        print('Creating the model...')
+        model = best_combined_model()
+        print('Model created!')
+
+        # autosave best Model
+        best_model_file = "../models/leafnet_v1.1_fold{}.h5".format(i+1)
+        best_model = ModelCheckpoint(best_model_file, monitor='val_loss', verbose=1, save_best_only=True)
+
+        print('Training model...')
+        history = model.fit_generator(combined_generator(fold_imgen, X_num_tr_fold),
+                                             samples_per_epoch=X_num_tr_fold.shape[0],
+                                             nb_epoch=100,
+                                             validation_data=([X_img_val_fold, X_num_val_fold], y_val_cat_fold),
+                                             nb_val_samples=X_num_val_fold.shape[0],
+                                             verbose=0,
+                                             callbacks=[best_model])
+
+        print('Calculating and storing min_val_loss...')
+        min_val_loss.append(np.min(history.history['val_loss']))
+
+    # See how good the top k splits were
+    k_best = sorted(range(n_folds), key=lambda i: min_val_loss[i])[:top_k]
+    k_best_avg = sum(k_best) / top_k
+    print('Got {} best avg of {} and needed {}'.format(top_k, k_best_avg, threshold))
+    if k_best_avg < threshold:
+        print('Done Training!')
+        break
+    print('Restarting Training...')
+
+
+
 
 # Now for the submission
 # Clear some memory
 kf, X_num_tr, X_img_tr, y_tr = (None,)*4
 yPred_proba = None
 ID, X_num_te, X_img_te = leaf99.load_test_data()
-for i in range(n_folds):
+for i in k_best:
 
     best_model_file = "../models/leafnet_v1.1_fold{}.h5".format(i + 1)
     print('Loading the best model fold {}/{}...'.format(i+1, n_folds))
